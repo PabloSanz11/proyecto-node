3e9fd6aa63ba6009d129cb0c06117cd438199092
PabloSanz11 <jpablosang11@gmail.com>
2020-11-26 19:19:16 -0600

    database

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ const util = require('util');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tallernode'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bb2bf4809bb124',
+    password: 'c29ea4b5',
+    database: 'heroku_9d29e9c8a90f9dd'
 });
 
 pool.query = util.promisify(pool.query);
